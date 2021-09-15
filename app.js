a2ac05ffcd9ebfd51a485c129469d8c1d35794b5
mikochung <48423524+mikochung@users.noreply.github.com>
2021-09-15 21:19:02 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,14 +4,25 @@ const Book = require('./routes/book')
 const app = Express()
 const cors = require('cors')
 
+
 app.use(BodyParser.json())
 app.use(BodyParser.urlencoded({extended : true}))
 
 app.use(cors())
 app.use('/',Book)
 
-const port = 10888
+app.set('port', (process.env.PORT || 5000));
 
-app.listen(process.env.port ||port, () => {
-    console.log('Server is up and running on port number')
-})
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+// const port = 10888
+// const port = process.env.PORT || 8080
+
+// app.listen(process.env.port || 80, () => {
+//     console.log('Server is up and running on port number')
+// })
+
+
